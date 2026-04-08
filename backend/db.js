a8754c1defc9826b474307cb172dacf3255dca63
Hareshwar patil <hareshwarpatil917@gmail.com>
2026-04-08 23:09:34 +0530

    connect railway db

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,18 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-  host: 'gondola.proxy.rlwy.net',
-  user: 'root',
-  password: 'HCAcRYAHXbkrdWKJgwuuZhTDuJpkIRNS',
-  database: 'railway',
-  port: 49586,
-  waitForConnections: true,
-  ssl: { rejectUnauthorized: false }
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 }).promise();
+
+db.query('SELECT 1')
+  .then(() => console.log('✅ DB Connected'))
+  .catch(err => console.log('❌ DB Error:', err.message));
 
 module.exports = db;
